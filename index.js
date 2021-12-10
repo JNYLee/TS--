28076e6d89b950dba29aa94657c563b71230a350
JNYLee <junyjuny1021@gmail.com>
2021-12-10 10:39:21 +0900

    [typescript/learn]: 타입스크립트 옵셔널 파라미터 구현

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log('Hellu');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const myName = "JNYLee", myAge = 21, myGender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello my name is ${name}, My age is ${age} and Im a ${gender}!`);
+};
+sayHi(myName, myAge);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('Hellu');
+const myName = "JNYLee",
+    myAge = 21,
+    myGender = 'male';
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello my name is ${name}, My age is ${age} and Im a ${gender}!`);
+};
+
+sayHi(myName, myAge);
+
+export {};
