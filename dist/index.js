c53ce5d5c2bb3d6fa7a289984ef00ff07b2aa44d
JNYLee <junyjuny1021@gmail.com>
2021-12-10 11:38:52 +0900

    [blockchain/init]: Blockchain Creating a Block

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const lynn = new Human("Lynn", 18, "female");
-const person = {
-    name: 'JNYLee',
-    age: 21,
-    gender: 'male'
-};
-const sayHi = (person) => {
-    return `Hello my name is ${person.name}, My age is ${person.age} and Im a ${person.gender}!`;
-};
-console.log(sayHi(lynn));
+const genesisBlock = new Block(0, "2020202020202", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
